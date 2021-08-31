9e0ba488161f718533a0fdb88a88f57263748921
Faris-ML <71413350+Faris-ML@users.noreply.github.com>
2021-08-31 22:05:38 +0300

    commit

--- Sentiment Analysis app/model.py
+++ Sentiment Analysis app/model.py
@@ -20,7 +20,13 @@ def downsample(df:pd.DataFrame, label_col_name:str) -> pd.DataFrame:
             .reset_index(drop=True)
             )
 
-def normalize(data_frame):
+def slice(df:pd.DataFrame,ratio:float):
+    num_rows=df.shape[0]
+    slice=int(np.ceil(num_rows*ratio))
+    df=df.sample(n=slice)
+    return df
+
+def normalize(data_frame:pd.DataFrame):
     min_max = preprocessing.MaxAbsScaler() # normalize data using min max scaler
     df_min_max = min_max.fit_transform(data_frame)
 
@@ -28,8 +34,8 @@ def normalize(data_frame):
 
 #initialize the models algorithms
 NBmodel=MultinomialNB()
-LRmodel=LogisticRegression(solver="saga",max_iter=10000,multi_class="ovr",C=100)
-SVMmodel=sk.svm.SVC(decision_function_shape="ovr",max_iter=10000,C=100)
+LRmodel=LogisticRegression(solver="saga",max_iter=10000,multi_class="ovr")
+SVMmodel=sk.svm.SVC(decision_function_shape="ovr",max_iter=10000)
 RFmodel=RandomForestClassifier(n_estimators=20,n_jobs=-1)
 
 dicm ={1:"naive bayas",2:"logistic regression",3:"SVM",4:"random forest"}
@@ -38,13 +44,14 @@ dicm ={1:"naive bayas",2:"logistic regression",3:"SVM",4:"random forest"}
 models=[NBmodel,LRmodel,SVMmodel,RFmodel]
 
 #load the text
-df=pd.read_csv("dataset_cleand.csv",encoding="utf-8")
+df=pd.read_csv("data sets/dataset_cleand.csv",encoding="utf-8")
 print(df.head())
 print(df["polarity"].value_counts())
-text=df["text"].astype("U").to_numpy()
+text=df["text"].astype("U").to_numpy().ravel()
 
 #under sampling the data
 df=downsample(df,"polarity")
+df=slice(df=df,ratio=0.05)
 print(df["polarity"].value_counts())
 
 #split lables and data
@@ -54,33 +61,41 @@ X=df["text"].astype("U").to_numpy()
 #split to train and test set
 X_train,X_test,y_train,y_test=sk.model_selection.train_test_split(X,Y,test_size=0.08)
 #make a list of n-gram text to number algorithm  to loop over it
+TF_IDF=[TfidfVectorizer(ngram_range=(1,1)).fit(text),TfidfVectorizer(ngram_range=(1,2)).fit(text),TfidfVectorizer(ngram_range=(1,3)).fit(text)]
 CV=[CountVectorizer(ngram_range=(1,1)).fit(text),CountVectorizer(ngram_range=(1,2)).fit(text),CountVectorizer(ngram_range=(1,3)).fit(text)]
-TF_IDF=[TfidfVectorizer(ngram_range=(1,1)).fit(text),TfidfVectorizer(ngram_range=(1,2)).fit(text),TfidfVectorizer(ngram_range=(1,3).fit(text))]
+
 
 #loop over models and text to number algorithims and pick the best score
 scores=np.zeros([4,2,3])
-for i in range(0,len(CV)):
-    x_train= normalize(CV[i].transform(X_train))
-    x_test= normalize(CV[i].transform(X_test))
-    for j in range(0,len(models)):
-        models[j].fit(x_train,y_train)
-        y_pred=models[j].predict(x_test)
-        y_pred2=models[j].predict(x_train)
-        scores[j,0,i]=sk.metrics.accuracy_score(y_true=y_test,y_pred=y_pred)
-        train_score=sk.metrics.accuracy_score(y_true=y_train,y_pred=y_pred2)
-        print("the model "+dicm[j+1]+" and countvectorizer "+str(i+1)+" gram accuray { train :"+str(train_score)+"-- test :"+str(scores[j,0,i])+" }")
 
 for k in range(0,len(TF_IDF)):
-    x_train = normalize(TF_IDF[i].transform(X_train))
-    x_test = normalize(TF_IDF[i].transform(X_test))
+    x_train = normalize(TF_IDF[k].transform(X_train.ravel()))
+    x_test = normalize(TF_IDF[k].transform(X_test.ravel()))
     for j in range(0,len(models)):
         models[j].fit(x_train, y_train)
-        y_pred = normalize(models[j].predict(x_test))
-        y_pred2 = normalize(models[j].predict(x_train))
-        scores[j, 1, i] = sk.metrics.accuracy_score(y_true=y_test, y_pred=y_pred)
+        y_pred = models[j].predict(x_test)
+        y_pred2 = models[j].predict(x_train)
+        scores[j, 1, k] = sk.metrics.accuracy_score(y_true=y_test, y_pred=y_pred)
         train_score = sk.metrics.accuracy_score(y_true=y_train, y_pred=y_pred2)
-        print("the model "+dicm[j+1]+" and TF-IDF "+str(i+1)+" gram accuray { train :"+str(train_score)+"-- test :"+str(scores[j,1,i])+" }")
-print(scores)
+        print("the model "+dicm[j+1]+" and TF-IDF "+str(k+1)+" gram accuray { train :"+str(train_score)+"-- test :"+str(scores[j,1,k])+" }")
+
+for i in range(0, len(CV)):
+    x_train = normalize(CV[i].transform(X_train.ravel()))
+    x_test = normalize(CV[i].transform(X_test.ravel()))
+    for j in range(0, len(models)):
+        models[j].fit(x_train, y_train)
+        y_pred = models[j].predict(x_test)
+        y_pred2 = models[j].predict(x_train)
+        scores[j, 0, i] = sk.metrics.accuracy_score(y_true=y_test, y_pred=y_pred)
+        train_score = sk.metrics.accuracy_score(y_true=y_train, y_pred=y_pred2)
+        print("the model " + dicm[j + 1] + " and countvectorizer " + str(i + 1) + " gram accuray { train :" + str(train_score) + "-- test :" + str(scores[j, 0, i]) + " }")
+
+TF_IDFscore=pd.DataFrame(columns=["1-gram","2-gram","3-gram"],index=["naive bayas","logistic regression","SVM","random forest"],data=scores[:,1,:])
+CVscore=pd.DataFrame(columns=["1-gram","2-gram","3-gram"],index=["naive bayas","logistic regression","SVM","random forest"],data=scores[:,0,:])
+print("TF-IDF accuracy : ")
+print(TF_IDFscore)
+print("CountVectorizer accuracy : ")
+print(CVscore)
 
 
 
