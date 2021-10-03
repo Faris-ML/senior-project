dacea332a8f205eb5b8f9a1f7b44d828897cd6e5
Faris-ML <71413350+Faris-ML@users.noreply.github.com>
2021-10-03 19:28:08 +0300

    commit

--- Sentiment Analysis app/data extraction.py
+++ Sentiment Analysis app/data extraction.py
@@ -1,14 +1,12 @@
 import pandas as pd
 
-ATT=pd.read_csv("data sets/ATT.csv",encoding="utf-8",index_col=0)
-HTL=pd.read_csv("data sets/HTL.csv",encoding="utf-8")
-MOV=pd.read_csv("data sets/MOV.csv",encoding="utf-8")
-PROD=pd.read_csv("data sets/PROD.csv",encoding="utf-8")
-RES=pd.read_csv("data sets/RES.csv",encoding="utf-8")
-RES1=pd.read_csv("data sets/RES1.csv",encoding="utf-8")
-RES2=pd.read_csv("data sets/RES2.csv",encoding="utf-8")
+HTL=pd.read_csv("data sets/old/HTL.csv",encoding="utf-8")
+MOV=pd.read_csv("data sets/old/MOV.csv",encoding="utf-8")
+PROD=pd.read_csv("data sets/old/PROD.csv",encoding="utf-8")
+RES=pd.read_csv("data sets/old/RES.csv",encoding="utf-8")
+RES1=pd.read_csv("data sets/old/RES1.csv",encoding="utf-8")
+RES2=pd.read_csv("data sets/old/RES2.csv",encoding="utf-8")
 
-print(ATT.head(),ATT.columns)
 print(HTL.head(),HTL.columns)
 print(MOV.head(),MOV.columns)
 print(PROD.head(),PROD.columns)
@@ -19,25 +17,21 @@ RES1.drop(columns=["user_id","restaurant_id"],inplace=True)
 RES1=RES1[["text","polarity"]]
 print(RES1.head(),RES1.columns)
 
-ATT.loc[ATT["polarity"]==1,"polarity"]=1
-ATT.loc[ATT["polarity"]==0,"polarity"]=2
-ATT.loc[ATT["polarity"]==-1,"polarity"]=3
-ATT.to_csv("data sets/ATT_.csv",index=False)
 
 HTL.loc[HTL["polarity"]==1,"polarity"]=1
 HTL.loc[HTL["polarity"]==0,"polarity"]=2
 HTL.loc[HTL["polarity"]==-1,"polarity"]=3
-HTL.to_csv("data sets/HTL_.csv",index=False)
+HTL.to_csv("data sets/new/hotels.csv",index=False)
 
 MOV.loc[MOV["polarity"]==1,"polarity"]=1
 MOV.loc[MOV["polarity"]==0,"polarity"]=2
 MOV.loc[MOV["polarity"]==-1,"polarity"]=3
-MOV.to_csv("data sets/MOV_.csv",index=False)
+MOV.to_csv("data sets/new/movies.csv",index=False)
 
 PROD.loc[PROD["polarity"]==1,"polarity"]=1
 PROD.loc[PROD["polarity"]==0,"polarity"]=2
 PROD.loc[PROD["polarity"]==-1,"polarity"]=3
-PROD.to_csv("data sets/PROD_.csv",index=False)
+PROD.to_csv("data sets/new/products.csv",index=False)
 
 REST=pd.concat([RES,RES1,RES2])
 
@@ -45,4 +39,4 @@ REST.loc[REST["polarity"]==1,"polarity"]=1
 REST.loc[REST["polarity"]==0,"polarity"]=2
 REST.loc[REST["polarity"]==-1,"polarity"]=3
 print(REST.head())
-REST.to_csv("data sets/RES_.csv",index=False)
+REST.to_csv("data sets/new/resturant.csv",index=False)
--- Sentiment Analysis app/data_preprocessing.py
+++ Sentiment Analysis app/data_preprocessing.py
@@ -14,51 +14,37 @@ def transform_text(text):
     # Arabic stop words with nltk
     stop_words = stopwords.words()
 
-    arabic_diacritics = re.compile("""
-                                 ّ    | # Shadda
-                                 َ    | # Fatha
-                                 ً    | # Tanwin Fath
-                                 ُ    | # Damma
-                                 ٌ    | # Tanwin Damm
-                                 ِ    | # Kasra
-                                 ٍ    | # Tanwin Kasr
-                                 ْ    | # Sukun
-                                 ـ     # Tatwil/Kashida
-                             """, re.VERBOSE)
-
     # remove punctuations
     translator = str.maketrans('', '', punctuations)
     text = text.translate(translator)
     #text=re.sub(punctuations,'',text)
-    # remove Tashkeel
-    text = re.sub(arabic_diacritics, '', text)
 
-    # remove longation
-    text = re.sub("[إأآا]", "ا", text)
-    text = re.sub("ى", "ي", text)
-    text = re.sub("ؤ", "ء", text)
-    text = re.sub("ئ", "ء", text)
-    text = re.sub("ة", "ه", text)
-    text = re.sub("گ", "ك", text)
 
     text = ' '.join(word for word in text.split() if word not in stop_words)
 
 
     return text
 
-df=pd.read_csv("dataset.csv")
-print("the data set info : \n",df.info,df.describe(),"\nthe data set shape : ",df.shape)
-print("null values check : ","\n",df.isnull().sum())
-plt.hist(df["polarity"],color="r",bins=10)
-plt.title("reviews distribution")
-plt.xlabel("sentiments")
-plt.ylabel("count")
-plt.show()
-print("the number of negative reviews : ",df[df["polarity"]==3]["polarity"].count())
-print("the number of natural reviews : ",df[df["polarity"]==2]["polarity"].count())
-print("the number of positive reviews : ",df[df["polarity"]==1]["polarity"].count())
-df['text']=df['text'].apply(transform_text)
-print(df.head())
-df.to_csv("dataset_cleand.csv",index=False)
+def main(datapath:string,filenames:list,savepath:string):
+    for i in filenames:
+        df = pd.read_csv(datapath + "/" + i + ".csv")
+        print("the data set info : \n", df.info, df.describe(), "\nthe data set shape : ", df.shape)
+        print("null values check : ", "\n", df.isnull().sum())
+        plt.hist(df["polarity"], color="r", bins=10)
+        plt.title("reviews distribution")
+        plt.xlabel("sentiments")
+        plt.ylabel("count")
+        plt.show()
+        print("the number of negative reviews : ", df[df["polarity"] == 3]["polarity"].count())
+        print("the number of natural reviews : ", df[df["polarity"] == 2]["polarity"].count())
+        print("the number of positive reviews : ", df[df["polarity"] == 1]["polarity"].count())
+        df['text'] = df['text'].apply(transform_text)
+        print(df.head())
+        df.to_csv(savepath+"/"+i+"_cleand.csv", index=False)
+
+filepath="data sets/new"
+file_names=["movies","hotels","resturant"]
+main(datapath=filepath,filenames=file_names,savepath="data sets")
+
 
 
